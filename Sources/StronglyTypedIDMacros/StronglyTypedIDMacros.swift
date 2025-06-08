b72c05a8233ab51fca1c70a6943d556b9cb8d7ee
John Wells <john.ivan@me.com>
2025-06-07 21:03:07 -0700

    Fix to accommodate required type argument format

--- Sources/StronglyTypedIDMacros/StronglyTypedIDMacros.swift
+++ Sources/StronglyTypedIDMacros/StronglyTypedIDMacros.swift
@@ -39,6 +39,7 @@ extension StronglyTypedIDMacro: DeclarationMacro {
         // Grab the second parameter (backing type).
         let backingIndex = arguments.index(after: arguments.startIndex)
         let backingArgument = arguments[backingIndex].expression
+        let backingTypeName = extractTypeArgument(backingArgument)
 
         // Check if there's adoption arguments and return a simplified declaration if not.
         let firstAdoptionIndex = arguments.index(after: backingIndex)
@@ -47,7 +48,7 @@ extension StronglyTypedIDMacro: DeclarationMacro {
             let result =
                 """
                 struct \(typeName): StronglyTypedID {
-                    var rawValue: \(backingArgument)
+                    var rawValue: \(backingTypeName)
                 }
                 """
             return ["\(raw: result)"]
@@ -55,14 +56,14 @@ extension StronglyTypedIDMacro: DeclarationMacro {
 
         // Grab the adoptions identifiers.
         let adoptions = arguments[firstAdoptionIndex...].map { element in
-            "\(element.expression)"
+            extractTypeArgument(element.expression)
         }
 
         // Build up result.
         let result =
             """
             struct \(typeName): StronglyTypedID, \(adoptions.joined(separator: ", ")) {
-                var rawValue: \(backingArgument)
+                var rawValue: \(backingTypeName)
             }
             """
         return ["\(raw: result)"]
@@ -75,6 +76,23 @@ extension StronglyTypedIDMacro: DeclarationMacro {
             // We're only supposed to call this with `StaticString` parameters, so if we're here it's toolset error or
             // out of sync macro declaration.
             preconditionFailure("Unexpected argument, toolset should only allow `StaticString` literals in")
+        }
+    }
+    
+    private static func extractTypeArgument(_ argument: ExprSyntax) -> String {
+        let memberAccess = argument.as(MemberAccessExprSyntax.self)
+        let declName = argument.as(DeclReferenceExprSyntax.self)
+        
+        if let memberAccess, memberAccess.declName.baseName.text == "self" {
+            if let name = memberAccess.base?.description {
+                return name
+            } else {
+                preconditionFailure("No backing type specified")
+            }
+        } else if let declName {
+            return declName.baseName.text
+        } else {
+            preconditionFailure("Arguments specifying types must be in the form of `T.self`")
         }
     }
 }
